3dd0bf0b139be9c97f930086e8e3cec7bc93dc61
pradeepkumar-28 <pk5324120@gmail.com>
2022-11-13 16:53:04 +0530

    add react-app and packages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { EuiProvider } from '@elastic/eui';
+
+function App() {
+  return (
+    <EuiProvider>
+      <div>App</div>
+    </EuiProvider>
+  )
+}
+
+export default App
